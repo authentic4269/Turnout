cad6fd5e3558587f8769a0aed30c17cf63efbd8f
tcl47 <tcl47@cornell.edu>
2013-05-10 15:10:02 -0400

    store in session

--- application.py
+++ application.py
@@ -172,6 +172,7 @@ def get_token():
         from urlparse import parse_qs
         r = requests.get('https://graph.facebook.com/oauth/access_token', params=params)
         token = parse_qs(r.content).get('access_token')
+        session['facebook_token'] = token
 
         return token
 
