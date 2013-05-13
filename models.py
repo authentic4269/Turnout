084ae9b85daa47cb61891ce66c5518ff21a1a17b
Mitchell Vogel <mjv58@cornell.edu>
2013-05-13 07:35:44 -0400

    hi

--- models.py
+++ models.py
@@ -45,7 +45,7 @@ class Reminder(db.Model):
 	user_id = db.Column(db.BigInteger, db.ForeignKey('users.fb_id'))
 	type = db.Column(db.Integer)
 	
-	def __init__(self, send_time, reminder_id, event_id, user_id, type)
+	def __init__(self, send_time, reminder_id, event_id, user_id, type):
 	self.send_time = send_time
 	self.reminder_id = reminder_id
 	self.event_id = event_id
