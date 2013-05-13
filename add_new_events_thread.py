b9b4f37c1586939300661f7e2d09fb2a03695272
Mitchell Vogel <mjv58@cornell.edu>
2013-05-13 02:20:21 -0400

    hi

--- add_new_events_thread.py
+++ add_new_events_thread.py
@@ -68,7 +68,7 @@ def get_new_events_one_user(db, user):
      return ret
 
 def is_in_future(dt):
-   d = datetime.strptime(dt['start_time'], "%Y-%m-%d" )
+   d = datetime.strptime(dt, "%Y-%m-%d" )
    if (datetime.now() - d) < timedelta(seconds=1): 
         return True
    return False
