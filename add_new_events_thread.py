82c6beec1aba5a78a3d89723f16d1318a175982a
Mitchell Vogel <mjv58@cornell.edu>
2013-05-13 08:27:59 -0400

    hi

--- add_new_events_thread.py
+++ add_new_events_thread.py
@@ -28,11 +28,12 @@ def run2(db):
         add_new_events_to_calendar(db, user, new_events_list)
 	for new_event in new_events_list:
 		newEvent = models.Event(new_event['name'], new_event['rsvp_status'], user.fb_id, new_event['id'], new_event['start_time'], new_event['end_time'], new_event['timezone'])
+		db.session.add(newEvent)
+		db.session.commit()
 		if user.remind_by_default == True:
 			r = models.Reminder(get_reminder_time(user, newEvent), p, new_event['id'], user.fb_id, user.remind_type)
 			db.session.add(r)
-		db.session.add(newEvent)
-    db.session.commit()
+			db.session.commit()
 
 def get_reminder_time(u, e):
 	d = datetime.strptime(e.start_time[0:10], "%Y-%m-%d" )	
